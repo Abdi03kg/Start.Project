eedcdd806223631c2dbde729972b0863aa04e301
shukurtabaldiev <shukurtabaldievicloud.com@MacBook-Air-shukurtabaldiev.local>
2024-05-23 12:52:42 +0600

    add script file

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 "use strict";
-const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
+
+const a = prompt("Сколько фильмов вы уже посмотрели?", "");
+const b = prompt("Один из последних просмотренных фильмов?", "");
+const c = prompt("Сколько фильмов вы уже посмотрели?", "");
+const d = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
